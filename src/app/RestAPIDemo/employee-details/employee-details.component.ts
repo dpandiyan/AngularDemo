3c9e8f0a006362aa9ed65ad9438ef6b9100d7eee
training <training@PC380344.cts.com>
2019-05-22 12:25:59 +0530

    back

--- src/app/RestAPIDemo/employee-details/employee-details.component.ts
+++ src/app/RestAPIDemo/employee-details/employee-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EmployeeService } from '../employee.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -12,7 +13,7 @@ export class EmployeeDetailsComponent implements OnInit {
   activeID: any
   employeeDetail: any
 
-  constructor(private activateRoute: ActivatedRoute, private empService: EmployeeService) { }
+  constructor(private activateRoute: ActivatedRoute, private empService: EmployeeService, private myRouter: Router) { }
 
   ngOnInit() {
     console.log(this.activateRoute)
@@ -23,4 +24,8 @@ export class EmployeeDetailsComponent implements OnInit {
       this.employeeDetail = response
     })
   }
+
+  goBack(){
+    this.myRouter.navigate(["/RESTService"])
+  }  
 }
